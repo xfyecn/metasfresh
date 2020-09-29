e410a024c724a7a4b6e4b044b58eb2ec72b068c4
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-09-29 09:02:26 +0300

    Some fixes on username

--- frontend/src/components/header/UserDropdown.js
+++ frontend/src/components/header/UserDropdown.js
@@ -141,7 +141,7 @@ class UserDropdown extends Component {
         >
           <Avatar id={me.avatarId} />
         </div> */}
-        <div className="header-item" onClick={() => handleUDOpen(true)}>
+        <div className="header-item header-tab" onClick={() => handleUDOpen(true)}>
           <span>{me.fullname}</span>
           <span className="header-left-spacing">
             <i className="far fa-chevron-down" />
